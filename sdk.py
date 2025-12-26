a8791ed8ac611fa931eff3ca8337599d3af5267c
Shehwaz81 <shehwazlongia@gmail.com>
2025-12-25 22:43:31 -0500

    added asychronous decoupled sdk v1

--- sdk.py
+++ sdk.py
@@ -1,18 +1,35 @@
-import threading
+import threading # threading allows python to send data without holding up the server, it is the *magic sauce*
 import requests
 import json
 
 class AltiLogger:
-    def __init__(self, api_url):
-        # TODO: Store the API URL and maybe setup a session
+    DEFAULT_URL = "something here"
+    def __init__(self, api_key, api_url=None):
+        if api_url:
+            self.api_url = api_url
+        else:
+            self.api_url = self.DEFAULT_URL
+        
+        self.session = requests.Session() #start session for this instance of the class
+        self.session.headers.update({
+            'content-type': 'application/json', #data that is being sent
+            'Authorization': f'Bearer {api_key}' #for authorization so that FastAPI knows who this user is, and where the data goes
+        })
         pass
 
-    def _send_payload(self, data):
-        # TODO: This method should run in a thread. 
-        # It needs to try/except the POST request to the API.
+    def _send_payload(self, data): # this function runs in a thread activated by the log func
+        try:
+            response = self.session.post(self.api_url, json=data) #use session instead of request.post so that we have the pre determined API key, etc
+        except Exception as e:
+            print(f"Alti Gaurd Error: Failed to send log. Reason: {e}")
         pass
 
     def log(self, input_text, output_text):
-        # TODO: Create the dictionary/JSON payload
-        # TODO: Start the thread targeting _send_payload
+        data = {
+            'input': input_text,
+            'output': output_text
+        }
+        thread = threading.Thread(target=self._send_payload, args=(data,))
+        thread.start()
+
         pass
